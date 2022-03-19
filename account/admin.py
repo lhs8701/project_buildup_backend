edb373dd285fd51669763ae49ca39c1d0553346b
hansol <hansol8701@gmail.com>
2022-03-19 20:30:49 +0900

    User & admin페이지 Custom

---
+++ account/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'nickname')
+    list_display_links = ['username', 'nickname']
+    search_fields = ['username', 'nickname']
+    fieldsets = (
+        ('ID & PW', {'fields': ('username', 'password')}),
+        ('Info', {'fields': ('nickname', 'date_joined')}),
+        ('Permissions', {'fields': ('is_active', 'is_superuser',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'nickname', 'password1', 'password2')}
+         ),
+    )
+
+
+admin.site.register(User, UserAdmin)
---
+++ account/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    nickname = models.CharField('닉네임', max_length=20)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.nickname
