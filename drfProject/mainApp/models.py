f23be0fa5450722ea5de02547f3e8b8010d7037b
hansol <hansol8701@gmail.com>
2022-03-19 15:22:48 +0900

    reset

--- drfProject/mainApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-# 간단하게 제목, 내용, 업데이트 된 날짜 담은 모델을 생성함
-class Review(models.Model):
-    title = models.CharField(max_length=50) #제목
-    content = models.TextField() #내용
-    updated_at = models.DateTimeField(auto_now=True) #업데이트 된 날짜
